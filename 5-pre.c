210dd6dd88a5c74f0757125cf6ff64cc0441eb79
Mafaz-2112 <140353697+Mafaz-2112@users.noreply.github.com>
2023-10-17 13:51:41 +0300

    Create 5-pre.c

---
+++ 5-pre.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * h_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+
+int h_precision(const char *format, int *i, va_list list)
+{
+    int precision = -1;
+	int c_i = *i + 1;
+
+	if (format[c_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (c_i += 1; format[c_i] != '\0'; c_i++)
+	{
+		if (is_digit(format[c_i]))
+		{
+			precision *= 10;
+			precision += format[c_i] - '0';
+		}
+		else if (format[c_i] == '*')
+		{
+			c_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = c_i - 1;
+
+	return (precision);
+}
