9b82d153cd3b6072510b715af0237ee03e031766
Mafaz-2112 <140353697+Mafaz-2112@users.noreply.github.com>
2023-10-17 18:48:34 +0300

    Update 6-size.c

--- 6-size.c
+++ 6-size.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * h_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * h_size - function to Calculates the size to cast the argument
+ * @format: Formatted arguments
+ * @i: arguments to be printed.
  *
  * Return: Precision.
  */
