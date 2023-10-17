2544dad146a914370d563e3b0b78c74a3ad2566e
Mafaz-2112 <140353697+Mafaz-2112@users.noreply.github.com>
2023-10-17 18:47:16 +0300

    Update 4-flag.c

--- 4-flag.c
+++ 4-flag.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * h_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * h_flags - function to Calculates active flags
+ * @format: Formatted arguments
+ * @i: parameter.
+ * Return: flags
  */
 int h_flags(const char *format, int *i)
 {
